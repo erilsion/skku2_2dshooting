d49df68f2cc124d116c83e6a2c404f5bbc0d560f
erilsion <erilsion@gmail.com>
2025-11-04 15:58:49 +0900

    실습 과제 (강사님과 함께 정리)

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -13,13 +13,13 @@ public class PlayerFire : MonoBehaviour
 
     [Header("총구")]
     public Transform FirePosition;
-    public Vector3 TwoBullet = new Vector2(0.5f, 0);
-    public Vector3 MiniBulletOffset = new Vector2(-1f, 0);
+    public Vector3 TwoBullet = new Vector2(0.5f, 0);  // FireOffset
+    public Vector3 MiniBulletOffset = new Vector2(-1f, 0);  // 추후 수정에는 총구 따로 만드는 게 좋다.
 
 
     [Header("쿨타임")]
     private float _fireTimer = 0f;
-    private float FireCooldown = 0.6f;
+    private float Cooltime = 0.6f;   // public const float Cooltime = 0.5f;
 
 
     [Header("자동 / 수동 공격")]
@@ -33,6 +33,7 @@ public class PlayerFire : MonoBehaviour
     private void Update()
     {
         _fireTimer += Time.deltaTime;
+
 
         switch (Input.GetKeyDown(AutoAtteck) || Input.GetKeyDown(AutoAtteck2))
         {
@@ -52,23 +53,10 @@ public class PlayerFire : MonoBehaviour
         }
 
         // 1. 발사 버튼을 누르고 있으면
-        if (Input.GetKey(KeyCode.Space) && _fireTimer > FireCooldown)
+        if (Input.GetKey(KeyCode.Space) && _fireTimer > Cooltime)
         {
-            // 2. 프리팹으로부터 총알(게임 오브젝트)을 생성한다.
-            // 유니티에서 게임 오브젝트를 생성할 때는 new가 Instantiate 라는 메서드를 이용한다.
-            // 클래스 -> 객체(속성+기능) -> 메모리에 로드된 객체를 인스턴스 => 인스턴스화
-            GameObject bullet1 = Instantiate(BulletPrefab);
-            GameObject bullet2 = Instantiate(BulletPrefab);
-            GameObject Minibullet1 = Instantiate(MiniBullet1Prefab);
-            GameObject Minibullet2 = Instantiate(MiniBullet2Prefab);
-
-
-            // 3. 총알 생성 후 위치를 플레이어 위치로 수정한다.
-            bullet1.transform.position = FirePosition.position + TwoBullet;
-            bullet2.transform.position = FirePosition.position - TwoBullet;
-            Minibullet1.transform.position = transform.position + MiniBulletOffset;
-            Minibullet2.transform.position = transform.position - MiniBulletOffset;
-
+            MakeBullets();
+            MakeSubBullets();
 
             // 4. 쿨타임 초기화
             _fireTimer = 0f;
@@ -76,19 +64,35 @@ public class PlayerFire : MonoBehaviour
     }
     public void AutoFireOn()
     {
-        if (_fireTimer > FireCooldown)
+        if (_fireTimer > Cooltime)
         {
-            GameObject bullet1 = Instantiate(BulletPrefab);
-            GameObject bullet2 = Instantiate(BulletPrefab);
-            GameObject Minibullet1 = Instantiate(MiniBullet1Prefab);
-            GameObject Minibullet2 = Instantiate(MiniBullet2Prefab);
-
-            bullet1.transform.position = FirePosition.position + TwoBullet;
-            bullet2.transform.position = FirePosition.position - TwoBullet;
-            Minibullet1.transform.position = transform.position + MiniBulletOffset;
-            Minibullet2.transform.position = transform.position - MiniBulletOffset;
+            MakeBullets();
+            MakeSubBullets();
 
             _fireTimer = 0f;
         }
+    }
+
+    private void MakeBullets()
+    {
+        // 2. 프리팹으로부터 총알(게임 오브젝트)을 생성한다.
+        // 유니티에서 게임 오브젝트를 생성할 때는 new가 Instantiate 라는 메서드를 이용한다.
+        // 클래스 -> 객체(속성+기능) -> 메모리에 로드된 객체를 인스턴스 => 인스턴스화
+        GameObject bullet1 = Instantiate(BulletPrefab);
+        GameObject bullet2 = Instantiate(BulletPrefab);
+
+
+        // 3. 총알 생성 후 위치를 플레이어 위치로 수정한다.
+        bullet1.transform.position = FirePosition.position + TwoBullet;
+        bullet2.transform.position = FirePosition.position - TwoBullet;
+    }
+
+    private void MakeSubBullets()
+    {
+        GameObject Minibullet1 = Instantiate(MiniBullet1Prefab);
+        GameObject Minibullet2 = Instantiate(MiniBullet2Prefab);
+
+        Minibullet1.transform.position = transform.position + MiniBulletOffset;
+        Minibullet2.transform.position = transform.position - MiniBulletOffset;
     }
 }
