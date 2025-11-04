1506d5c108956d294203854eed7310a5c27fb237
erilsion <erilsion@gmail.com>
2025-11-04 13:40:36 +0900

    총알 쿨타임 적용

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -11,21 +11,43 @@ public class PlayerFire : MonoBehaviour
 
     [Header("총구")]
     public Transform FirePosition;
+    public Vector3 TwoBullet = new Vector2(1, 0);
+
+
+    [Header("쿨타임")]
+    private float _fireTimer = 0f;
+    private float FireCooldown = 0.6f;
+
+
+    [Header("자동 / 수동 공격")]
+    private KeyCode AutoAtteck = KeyCode.Keypad1;
+    private KeyCode NotAutoAtteck = KeyCode.Keypad2;
 
 
     private void Update()
     {
+        _fireTimer += Time.deltaTime;
+        if (Input.GetKeyDown())
+        {
+
+        }
         // 1. 발사 버튼을 누르고 있으면
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && _fireTimer > FireCooldown)
         {
             // 2. 프리팹으로부터 총알(게임 오브젝트)을 생성한다.
             // 유니티에서 게임 오브젝트를 생성할 때는 new가 Instantiate 라는 메서드를 이용한다.
             // 클래스 -> 객체(속성+기능) -> 메모리에 로드된 객체를 인스턴스 => 인스턴스화
-            GameObject bullet = Instantiate(BulletPrefab);
+            GameObject bullet1 = Instantiate(BulletPrefab);
+            GameObject bullet2 = Instantiate(BulletPrefab);
 
 
             // 3. 총알 생성 후 위치를 플레이어 위치로 수정한다.
-            bullet.transform.position = FirePosition.position;
+            bullet1.transform.position = FirePosition.position + TwoBullet;
+            bullet2.transform.position = FirePosition.position - TwoBullet;
+
+
+            // 4. 쿨타임 초기화
+            _fireTimer = 0f;
         }
     }
 }
