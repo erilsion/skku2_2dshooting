020551577990aef05e0741033e568aa7b7686e21
erilsion <erilsion@gmail.com>
2025-11-04 09:46:06 +0900

    5. R누르면 원점으로 이동하기

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -15,18 +15,30 @@ public class PlayerMove : MonoBehaviour
     [Header("능력치")]
     public float Speed = 3f;   // 초당 3유닛(3칸) 이동
 
+
     [Header("이동 제한 범위")]
     public float MinX = -2.5f;
     public float MaxX = 2.5f;
     public float MinY = -5f;
     public float MaxY = 0f;
-    public KeyCode Origin = KeyCode.R;   // R 누르면 원점으로 돌아가도록
+
+
+    [Header("시작위치")]
+    private Vector3 _originPosition;  // 은닉화-prvate로 해준다.
+
+    private void Start()
+    {
+        // 처음 시작 위치 저장
+        _originPosition = transform.position;
+    }
+
 
     [Header("스피드 증감")]
     public float speedIncrease = 1f;
     public float speedDecrease = -1f;
     public KeyCode speedUpKey = KeyCode.Q;
     public KeyCode speedDownKey = KeyCode.E;
+    public KeyCode GoBack = KeyCode.R;
     public float maxSpeed = 10f;
     public float minSpeed = 1f;
 
@@ -34,13 +46,7 @@ public class PlayerMove : MonoBehaviour
     // 게임 오브젝트가 게임을 시작한 후 최대한 많이
     private void Update()
     {
-        // 1. 키보드 입력을 감지한다.
-        // 유니티에서는 Input이라는 모듈이 입력에 관한 모든 것을 담당한다. (버튼을 눌렀냐, 키보드를 입력했냐)
-        float h = Input.GetAxis("Horizontal"); // 수평 입력에 대한 값을 -1 ~ 0 ~ 1로 가져온다. (가속도 존재)
-        float v = Input.GetAxis("Vertical"); // 수직 입력에 대한 값을 -1 ~ 0 ~ 1로 가져온다.
 
-        // Input.GetAxisRaw: 딜레이 없이 바로 반응 (가속도 X)
-        // float h = Input.GetAxisRaw("Horizontal"); 로 하면 천천히 가속되는게 아니라 바로 1이나 -1로 감지된다.
 
 
         // GetKey: 키가 눌려있는지 계속 감지
@@ -55,6 +61,12 @@ public class PlayerMove : MonoBehaviour
         else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             Speed = 3f;
+        }
+
+        if (Input.GetKey(GoBack))
+        {
+            TranslateToOrigin();
+            return;
         }
 
         // Speed = Mathf.Clamp(Speed, minSpeed, maxSpeed); - 속도를 최소값과 최대값 사이로 반환해준다.
@@ -83,7 +95,17 @@ public class PlayerMove : MonoBehaviour
             }
         }
 
-        Debug.Log($"h: {h}, v: {v}");
+        
+
+        // 1. 키보드 입력을 감지한다.
+        // 유니티에서는 Input이라는 모듈이 입력에 관한 모든 것을 담당한다. (버튼을 눌렀냐, 키보드를 입력했냐)
+        float h = Input.GetAxis("Horizontal"); // 수평 입력에 대한 값을 -1 ~ 0 ~ 1로 가져온다. (가속도 존재)
+        float v = Input.GetAxis("Vertical"); // 수직 입력에 대한 값을 -1 ~ 0 ~ 1로 가져온다.
+
+
+
+        // Input.GetAxisRaw: 딜레이 없이 바로 반응 (가속도 X)
+        // float h = Input.GetAxisRaw("Horizontal"); 로 하면 천천히 가속되는게 아니라 바로 1이나 -1로 감지된다.
 
 
         // 2. 입력으로부터 방향을 구한다.
@@ -141,4 +163,14 @@ public class PlayerMove : MonoBehaviour
         transform.position = newPosition;       // 새로운 위치로 갱신
 
     }
+
+    private void TranslateToOrigin()
+    {
+        // 1. 입력을 받는다.
+        // 2. 방향을 구한다.
+        Vector2 direction = _originPosition - transform.position;
+        // 3. 이동을 한다.
+        transform.Translate(translation: direction * Speed * Time.deltaTime);
+    }
 }
+
