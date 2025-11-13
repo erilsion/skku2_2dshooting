c079d07bd13693638687e74e14c2a24fafff748a
erilsion <erilsion@gmail.com>
2025-11-13 11:13:17 +0900

    Fix: Save 및 Load 기능을 JSON으로 수정
    Fix: Save 및 Load 기능을 JSON으로 적용하고 개선

--- Assets/02.Scripts/Manager/ScoreManager.cs
+++ Assets/02.Scripts/Manager/ScoreManager.cs
@@ -3,6 +3,12 @@ using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 using DG.Tweening;
 
+[System.Serializable]
+public class  UserData
+{
+    public int currentScore;
+    public int highScore;
+}
 
 public class ScoreManager : MonoBehaviour
 {
@@ -54,16 +60,37 @@ public class ScoreManager : MonoBehaviour
 
     private void Save()
     {
-        PlayerPrefs.SetInt(ScoreKey, _currentScore);
-        if (_currentScore >= _highScore)
-        {
-            PlayerPrefs.SetInt(ScoreKey, _highScore);
-        }
+        UserData data = new UserData();
+        data.currentScore = _currentScore;
+        data.highScore = _highScore;
+
+        string json = JsonUtility.ToJson(data);
+
+        PlayerPrefs.SetString(ScoreKey, json);
+        PlayerPrefs.Save();
+
+        Debug.Log("점수 저장 완료: " + json);
     }
 
     private void Load()
     {
-        _highScore = PlayerPrefs.GetInt(ScoreKey, _highScore);
-        _currentScore = 0;
+        if (PlayerPrefs.HasKey(ScoreKey))
+        {
+            string json = PlayerPrefs.GetString(ScoreKey);
+
+            UserData data = JsonUtility.FromJson<UserData>(json);
+
+            _highScore = data.highScore;
+            _currentScore = 0;
+
+            Debug.Log("점수 불러오기 완료: " + json);
+        }
+        else
+        {
+            _highScore = 0;
+            _currentScore = 0;
+
+            Debug.Log("저장된 점수가 없어 새로 시작합니다.");
+        }
     }
 }
