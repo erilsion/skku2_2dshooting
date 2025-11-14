2674c29174ac6b4cd4f8c2db85cd9897077cf497
erilsion <erilsion@gmail.com>
2025-11-14 10:38:53 +0900

    Fix: 필살기 버그 수정
    Fix: 필살기 버그 수정
      - 필살기가 제대로 작동하지 않던 버그 수정

--- Assets/02.Scripts/Bullet/SpecialBomb.cs
+++ Assets/02.Scripts/Bullet/SpecialBomb.cs
@@ -7,25 +7,38 @@ public class SpecialBomb : MonoBehaviour
 
     [Header("쿨타임")]
     private float _bombTimer = 0f;
-    private float _finishTimer = 3f;
+    private float _finishTime = 3f;
     private bool _finished = false;
+    private float _effectTimer = 0f;
+    private float _effectFinishTime = 4f;
 
     [Header("파티클 프리팹")]
     public GameObject PlayerEffectPrefab;
-    public GameObject ParticlePrefab;
+    public GameObject ParticlePrefab1;
+    public GameObject ParticlePrefab2;
+    public GameObject DestroyEffectPrefab;
+
+    [Header("플레이어 위치")]
+    private GameObject _playerObject;
+
 
     private void Start()
     {
+        _playerObject = GameObject.FindWithTag("Player");
         _bombTimer = 0f;
-        _finishTimer = 3f;
+        _finishTime = 3f;
+        _effectTimer = 0f;
+        _effectFinishTime = 4f;
+        MakeParticleEffect();
     }
 
     public void Update()
     {
         _bombTimer += Time.deltaTime;
 
-        if (_bombTimer >= _finishTimer && !_finished)
+        if (_bombTimer >= _finishTime && !_finished)
         {
+            _effectTimer += Time.deltaTime;
             Explode();
         }
     }
@@ -33,11 +46,6 @@ public class SpecialBomb : MonoBehaviour
     private void Explode()
     {
         _finished = true;
-
-        if (SpecialBombPrefab != null)
-        {
-            MakeParticleEffect();
-        }
         FinishSpecial();
     }
 
@@ -48,16 +56,24 @@ public class SpecialBomb : MonoBehaviour
 
         Destroy(other.gameObject);
     }
+
     private void MakeParticleEffect()
     {
-        Instantiate(PlayerEffectPrefab, transform.position, Quaternion.identity);
-        Instantiate(ParticlePrefab, transform.position, Quaternion.identity);
+        PlayerEffectPrefab = Instantiate(PlayerEffectPrefab, _playerObject.transform.position, Quaternion.identity);
+        ParticlePrefab1 = Instantiate(ParticlePrefab1, transform.position, Quaternion.identity);
+        ParticlePrefab2 = Instantiate(ParticlePrefab2, transform.position, Quaternion.identity);
     }
 
     private void FinishSpecial()
     {
         if (PlayerEffectPrefab != null) Destroy(PlayerEffectPrefab);
-        if (ParticlePrefab != null) Destroy(ParticlePrefab);
+        if (ParticlePrefab1 != null) Destroy(ParticlePrefab1);
+        if (ParticlePrefab2 != null) Destroy(ParticlePrefab2);
+        DestroyEffectPrefab = Instantiate(DestroyEffectPrefab, transform.position, Quaternion.identity);
         Destroy(gameObject);
+        if (_effectTimer >= _effectFinishTime)
+        {
+            Destroy(DestroyEffectPrefab);
+        }
     }
 }
